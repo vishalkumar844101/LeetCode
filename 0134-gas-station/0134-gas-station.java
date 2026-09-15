75e3d3d829cfd83da8b087f1e41d86bc7ba0d720
VISHAL KUMAR <123533369+vishalkumar844101@users.noreply.github.com>
2026-09-16 05:02:31 +0530

    Time: 3 ms (30.67%), Space: 119.5 MB (26.37%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+
+        // for(int val : gas){
+        //     totalGas+=val;
+        // }
+
+        // for(int val : cost){
+        //     totalCost += val;
+        // }
+
+        // if(totalGas<totalCost){
+        //     return -1;
+        // }
+
+        // unique sol always exists
+
+       int start = 0;
+       int currGas = 0;
+       for(int i = 0; i<gas.length; i++){
+        totalGas += gas[i];
+        totalCost += cost[i];
+        currGas = currGas + gas[i] - cost[i];
+        if(currGas<0){
+            start = i + 1;
+            currGas = 0;
+        }
+        
+       
+      }
+      return totalGas<totalCost ? -1 : start ;
+      
+
+
+
+        
+    }
+}
